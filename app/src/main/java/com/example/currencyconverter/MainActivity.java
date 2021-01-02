8bd9953eb60b022d7986a0f43e9275140694039f
Atul Sharma <atulsharma20may@gmail.com>
2021-01-02 19:58:26 +0530

    inittial commit

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convert(View view) {
+        EditText dollarAmount = findViewById(R.id.editText);
+        TextView inrAmount = findViewById(R.id.textView2);
+
+        int dollar = Integer.parseInt(dollarAmount.getText().toString());
+        int inr = dollar*77;
+
+        inrAmount.setText(inr);
+    }
+
+    public void reset(View view) {
+        EditText dollarAmount = findViewById(R.id.editText);
+        TextView inrAmount = findViewById(R.id.textView2);
+
+        dollarAmount.setText("");
+        inrAmount.setText("");
     }
 }
